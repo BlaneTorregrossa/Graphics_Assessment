75f36bd5ebc3fc384a0a2ef3e2780c7541eb09a2
BlaneTorregrossa <blanet97@cox.net>
2017-08-18 08:54:34 -0500

    removed gizmos and it works again

--- bootstrap/bootstrap/Main.cpp
+++ bootstrap/bootstrap/Main.cpp
@@ -5,11 +5,6 @@
 #include <glm\glm\vec3.hpp>
 #include <glm\glm\vec4.hpp>
 #include <assert.h>
-#include <glm\glm\mat4x4.hpp>
-
-#include "Gizmos.h"
-#include <glm\glm\glm.hpp>
-#include <glm\glm\ext.hpp>
 
 #include <iostream>
 
@@ -22,7 +17,7 @@ int main()
 		printf("glfw is not avalible");
 	}
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Hi my name is Window", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hi my name is Window", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		glfwTerminate();
@@ -42,6 +37,7 @@ int main()
 	auto major = ogl_GetMajorVersion();
 	auto minor = ogl_GetMinorVersion();
 
+	printf("GL: %i.%i\n", major, minor);
 	//Sets color for the window
 	glClearColor(0.12f, 0.12f, 0.30f, 1.0f);
 	glm::vec4 clearcolor = glm::vec4(0.12f, 0.12f, 0.30f, 1.0f);
@@ -50,6 +46,7 @@ int main()
 		//Clears BUffers for Color and depth
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// a load of if statements mostly to change window color on input
 		if (glfwWindowShouldClose(window))
 			break;
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
@@ -67,25 +64,6 @@ int main()
 			clearcolor.b = 0.30f;
 		}
 
-		Gizmos::clear();
-		Gizmos::addTransform(glm::mat4(1));
-		glm::vec4 white(1);
-		glm::vec4 black(0, 0, 0, 1);
-
-		glm::mat4 view = glm::lookAt(glm::vec3(10, 10, 10), glm::vec3(0), glm::vec3(0, 1, 0));
-		glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
-		for (int i = 0; i < 21; i++)
-		{
-			Gizmos::create();
-			Gizmos::addLine(glm::vec3(-10 + i, 0, 10),
-				glm::vec3(-10 + i, 0, -10),
-				i == 10 ? white : black);
-
-			Gizmos::addLine(glm::vec3(10, 0, -10 + i),
-				glm::vec3(10, 0, -10 + i),
-				i == 10 ? white : black);
-		}
-		Gizmos::draw(projection * view);
 		//Colors Drawn. Can't be modified/updated without this
 		glClearColor(clearcolor.r, clearcolor.g, clearcolor.b, clearcolor.a);
 		glfwPollEvents();
